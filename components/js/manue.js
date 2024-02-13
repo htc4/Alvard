a8716187770d9559ce7e5a762129f3dffc0ac0bb
Alvard <alvardhovakimyan6@gmail.com>
2024-02-13 16:14:33 +0400

    added landing page full style

---
+++ components/js/manue.js
@@ -0,0 +1,15 @@
+console.log("Menu script loaded successfully.");
+
+// Function to navigate to the shop page
+function go_shop() {
+    window.location.assign("../../shop/shop.html");
+}
+function go_events() {
+    window.location.href = "../../shop/shop.html";
+}
+function go_card() {
+    window.location.href = "../../shop/shop.html";
+}
+function go_log_in() {
+    window.location.href = "../../shop/shop.html";
+}
