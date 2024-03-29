92342a0967b85a5638ba540d405fbb35324d72df
Alvard <alvardhovakimyan6@gmail.com>
2024-03-29 14:12:20 +0400

    added main structura and login page

---
+++ client/components/login/loginbutton.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+    createLoginButton();
+});
+
+function createLoginButton() {
+    if (!document.getElementById('logbutton')) {
+        var button = document.createElement('button');
+        button.setAttribute('id', 'logbutton');
+        button.innerHTML = 'Login';
+        var container = document.querySelector('.login-form__footer');
+        container.appendChild(button);
+        console.log('Login button created');
+        button.addEventListener('click', handleLogin); // Attach click event listener
+    }
+}
+
+function handleLogin() {
+    console.log('Login button clicked');
+    // Call the authentication function when login button is clicked
+    authenticateUser();
+}
+
+// Function to handle user authentication
+function authenticateUser() {
+    var email = document.getElementById('emailInput').value;
+    var password = document.getElementById('passwordInput').value;
+
+    // Define your SQL query for authentication
+    const selectQuery = 'SELECT * FROM "user" WHERE email = $1 AND password = $2';
+
+    // Data for the query
+    const data = [email, password];
+
+    // Execute the query
+    pool.query(selectQuery, data, (err, res) => {
+        if (err) {
+            console.error('Error executing query', err.stack);
+            return;
+        }
+
+        // If user exists and password matches
+        if (res.rows.length > 0) {
+            // Redirect to mai.html
+            window.location.href = 'mai.html';
+        } else {
+            // Handle authentication failure
+            console.log('Authentication failed. Invalid email or password.');
+        }
+    });
+}
